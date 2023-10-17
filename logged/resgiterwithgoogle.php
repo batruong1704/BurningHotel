c68e9a609a3d7bf2b824636254b8d4fc2ac9ff18
batruong1704 <batruong1704@gmail.com>
2023-10-18 01:50:39 +0700

    final function login with google

--- logged/resgiterwithgoogle.php
+++ logged/resgiterwithgoogle.php
@@ -34,7 +34,7 @@
             $pass = $row['PassWord'];
         }
     }
-    if (isset($_POST['submit_confirm'])) {                                 // Tại button xác nhận thông tin
+    if (isset($_POST['submit_confirm'])) { // Tại button xác nhận thông tin
         $hoten = $_POST["ten"];
         $email = $_POST["email"];
         $sdt = $_POST["sdt"];
@@ -49,7 +49,7 @@
         $result = mysqli_query($con, $sql);
         if ($result == true) {
             echo "<script>";
-            echo "alert('Đăng kí thành công !!!');";
+            echo "alert('❤️ Đăng kí thành công ❤️');";
             echo "window.location.href=' home.php'";
             echo "</script>";
             header("Location: home.php");
@@ -59,7 +59,7 @@
             echo "window.location=javascript: history.go(-1)";
             echo "</script>";
         }
-    } else if (isset($_POST['submit_huy'])) {                                 // Tại button xác nhận thông tin
+    } else if (isset($_POST['submit_huy'])) { // Tại button xác nhận thông tin
         $con = mysqli_connect("localhost", "root", "", "burninghotel");
         if (!$con) {
             echo "Kết nối thất bại";
@@ -70,7 +70,7 @@
         $result = mysqli_query($con, $sql);
         if ($result == true) {
             echo "<script>";
-            echo "alert('Đăng kí thất bại !!!');";
+            echo "alert('Bạn đã huỷ đăng ký, chúng tôi sẽ điều hướng bạn về trang đăng nhập 😭');";
             echo "window.location.href='../index.php'";
             echo "</script>";
             header("Location: ../index.php");
@@ -96,7 +96,8 @@
     <section id="infor">
         <div class="container">
             <h1 class="text-center" style="margin-top: 25px;">Tạo Tài Khoản</h1>
-            <p class="text-center">Thông tin được cung cấp bên dưới sẽ được sử dụng để đăng nhập vào tài khoản của khách sạn cho nhu cầu đặt phòng của bạn.</p>
+            <p class="text-center">Thông tin được cung cấp bên dưới sẽ được sử dụng để đăng nhập vào tài khoản của khách
+                sạn cho nhu cầu đặt phòng của bạn.</p>
             <form action="" method="POST">
                 <div class="row">
                     <div class="col-5">
@@ -138,97 +139,120 @@
         </div>
     </section>
 
-    <?php
-        include('footer.php');
-    ?>
+    <?php include('footer.php'); ?>
+    
+
     <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            const form = document.querySelector("form");
-            const submitButton = form.querySelector("[name='submit_confirm']");
-            const inputs = form.querySelectorAll("input");
+    document.addEventListener("DOMContentLoaded", function() {
+        const form = document.querySelector("form");
+        const submitButtonConfirm = form.querySelector("[name='submit_confirm']");
+        const submitButtonHuy = form.querySelector("[name='submit_huy']");
+        const inputs = form.querySelectorAll("input");
+        let errorDisplayed = false;
 
-            form.addEventListener("submit", function(event) {
-                let valid = true;
+        inputs.forEach(function(input) {
+            input.addEventListener("input", function(event) {
+                validateInput(input);
+            });
+        });
 
-                inputs.forEach(function(input) {
-                    const value = input.value.trim();
-                    if (value === "") {
-                        valid = false;
-                        input.classList.add("error");
-                        showErrorMessage(input, "Vui lòng nhập giá trị.");
-                    } else {
-                        input.classList.remove("error");
-                        hideErrorMessage(input);
-                    }
+        function validateInput(input) {
+            const value = input.value.trim();
+            const errorElement = input.parentNode.querySelector(".error-message");
+            input.classList.remove("error");
+            if (errorElement) {
+                errorElement.remove();
+            }
 
-                    if (input.name === "sdt" && !isValidPhoneNumber(value)) {
-                        valid = false;
-                        input.classList.add("error");
-                        showErrorMessage(input, "Số điện thoại không hợp lệ.");
-                    }
+            if (value === "") {
+                showErrorMessage(input, "Không được bỏ trống!");
+            } else {
+                if (input.name === "sdt" && !isValidPhoneNumber(value)) {
+                    showErrorMessage(input, "Số điện thoại không hợp lệ.");
+                } else if (input.name === "cmnd" && !isValidCMND(value)) {
+                    showErrorMessage(input, "Số CMND không hợp lệ. (bao gồm 9 số)");
+                } else if (input.name === "email" && !isValidEmail(value)) {
+                    showErrorMessage(input, "Email không hợp lệ.");
+                } else if (input.name === "pass" && !isValidPassword(value)) {
+                    showErrorMessage(input, "Mật khẩu phải có ít nhất 8 ký tự.");
+                }
+            }
+        }
 
-                    if (input.name === "cmnd" && !isValidCMND(value)) {
-                        valid = false;
-                        input.classList.add("error");
-                        showErrorMessage(input, "Số CMND không hợp lệ.");
-                    }
+        submitButtonConfirm.addEventListener("click", function(event) {
+            errorDisplayed = false;
+            let valid = true;
 
-                    if (input.name === "email" && !isValidEmail(value)) {
-                        valid = false;
-                        input.classList.add("error");
-                        showErrorMessage(input, "Email không hợp lệ.");
-                    }
+            inputs.forEach(function(input) {
+                validateInput(input);
 
-                    if (input.name === "pass" && !isValidPassword(value)) {
-                        valid = false;
-                        input.classList.add("error");
-                        showErrorMessage(input, "Mật khẩu phải có ít nhất 8 ký tự.");
-                    }
-                });
-
-                if (!valid) {
-                    event.preventDefault();
+                if (input.classList.contains("error") && !errorDisplayed) {
+                    errorDisplayed = true;
+                    valid = false;
                 }
             });
 
-            function isValidPhoneNumber(phoneNumber) {
-                // Kiểm tra định dạng số điện thoại (ví dụ: 1234567890)
-                const phoneRegex = /^[0-9]{10}$/;
-                return phoneRegex.test(phoneNumber);
-            }
-
-            function isValidCMND(cmnd) {
-                // Kiểm tra định dạng số CMND (ví dụ: 123456789)
-                const cmndRegex = /^[0-9]{9}$/;
-                return cmndRegex.test(cmnd);
-            }
-
-            function isValidEmail(email) {
-                // Kiểm tra định dạng email
-                const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-                return emailRegex.test(email);
-            }
-
-            function isValidPassword(password) {
-                // Kiểm tra mật khẩu có ít nhất 8 ký tự
-                return password.length >= 8;
-            }
-
-            function showErrorMessage(input, message) {
-                const errorElement = document.createElement("div");
-                errorElement.className = "error-message";
-                errorElement.textContent = message;
-                input.parentNode.appendChild(errorElement);
-            }
-
-            function hideErrorMessage(input) {
-                const errorElement = input.parentNode.querySelector(".error-message");
-                if (errorElement) {
-                    errorElement.remove();
-                }
+            if (!valid) {
+                event.preventDefault();
             }
         });
-    </script>
+
+
+        function isValidPhoneNumber(phoneNumber) {
+            // Kiểm tra định dạng số điện thoại (ví dụ: 1234567890)
+            const phoneRegex = /^[0-9]{10}$/;
+            return phoneRegex.test(phoneNumber);
+        }
+
+        function isValidCMND(cmnd) {
+            // Kiểm tra định dạng số CMND (ví dụ: 123456789)
+            const cmndRegex = /^[0-9]{9}$/;
+            return cmndRegex.test(cmnd);
+        }
+
+        function isValidEmail(email) {
+            // Kiểm tra định dạng email
+            const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+            return emailRegex.test(email);
+        }
+
+        function isValidPassword(password) {
+            // Kiểm tra mật khẩu có ít nhất 8 ký tự
+            return password.length >= 8;
+        }
+
+        function showErrorMessage(input, message) {
+            // Kiểm tra xem thông báo lỗi đã tồn tại chưa
+            const errorElement = input.parentNode.querySelector(".error-message");
+            if (errorElement) {
+                errorElement.textContent = message;
+            } else {
+                // Nếu chưa tồn tại, tạo mới và thêm vào trang
+                const newErrorElement = document.createElement("div");
+                newErrorElement.className = "error-message";
+                newErrorElement.textContent = message;
+                input.parentNode.appendChild(newErrorElement);
+            }
+        }
+        function hideErrorMessage(input) {
+            const errorElement = input.parentNode.querySelector(".error-message");
+            if (errorElement) {
+                errorElement.remove();
+            }
+        }
+        // tạo và hiển thị thông báo lỗi cho trường nhập liệu cụ thể khi có lỗi xảy ra
+        function showErrorMessage(input, message) {
+            input.classList.add("error");
+            const errorElement = document.createElement("div");
+            errorElement.className = "error-message";
+            errorElement.textContent = message;
+            input.parentNode.appendChild(errorElement);
+        }
+    });
+</script>
+
+
+
 
     <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
     <!-- bootstrap -->
