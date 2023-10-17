254a0df345688ddd2cf8fc3ec21574855d6c9768
Dung Nguyễn <nguyenthuydungtstb@gmail.com>
2023-10-17 16:37:12 +0700

    thanhtoan

---
+++ logged/CapNhapTrangThaiPhong.php
@@ -0,0 +1,13 @@
+<?php
+    $con = mysqli_connect("localhost", "root", "", "quanlykhachsan");
+    if (!$con) {
+        echo 'Kết nối không thành công';
+    }
+    $maphong = $_GET['maphong'];
+    $sql = "UPDATE phong SET TrangThai='Trống' WHERE MaPhong='" . $maphong . "'";
+    if (mysqli_query($con, $sql)) {
+        echo 'Cập nhật thành công';
+    } else {
+        echo 'Cập nhật không thành công' ;
+    }
+?>
