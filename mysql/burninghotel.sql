a35b64397ba3f39911c664cbdcabd76ab9de9e9e
batruong1704 <batruong1704@gmail.com>
2023-10-17 23:31:20 +0700

    upgrade sql

--- mysql/burninghotel.sql
+++ mysql/burninghotel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 16, 2023 lúc 10:29 AM
--- Phiên bản máy phục vụ: 10.4.28-MariaDB
--- Phiên bản PHP: 8.2.4
+-- Host: 127.0.0.1
+-- Generation Time: Oct 17, 2023 at 05:19 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `burninghotel`
+-- Database: `quanlykhachsan`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `chitietdatmon`
+-- Table structure for table `chitietdatmon`
 --
 
 CREATE TABLE `chitietdatmon` (
@@ -33,77 +33,93 @@ CREATE TABLE `chitietdatmon` (
   `MaMonAn` int(11) NOT NULL,
   `SoLuong` int(11) NOT NULL,
   `ThanhTien` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `chitietdatphong`
+-- Table structure for table `chitietdatphong`
 --
 
 CREATE TABLE `chitietdatphong` (
-  `MaDatPhong` int(11) NOT NULL,
-  `MaKhachHang` int(11) NOT NULL,
-  `MaHoaDon` int(11) NOT NULL,
+  `MaCTDP` int(11) NOT NULL,
+  `MaPDP` int(11) NOT NULL,
   `MaPhong` int(11) NOT NULL,
   `NgayDen` date NOT NULL,
-  `NgayDi` date NOT NULL,
-  `TienPhong` int(11) NOT NULL,
-  `DaThanhToan` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `NgayDi` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `chitietdatphong`
+--
+
+INSERT INTO `chitietdatphong` (`MaCTDP`, `MaPDP`, `MaPhong`, `NgayDen`, `NgayDi`) VALUES
+(1, 1, 1, '2023-10-18', '2023-10-19');
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `chitietdichvu`
+-- Table structure for table `chitietdichvu`
 --
 
 CREATE TABLE `chitietdichvu` (
-  `MaDatDichVu` int(11) NOT NULL,
-  `MaDichVu` int(11) NOT NULL,
-  `TongTien` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `MaCTDV` int(11) NOT NULL,
+  `MaPDV` int(11) NOT NULL,
+  `MaDichVu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `chitietdichvu`
+--
+
+INSERT INTO `chitietdichvu` (`MaCTDV`, `MaPDV`, `MaDichVu`) VALUES
+(1, 1, 1),
+(2, 1, 2);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `dichvu`
+-- Table structure for table `dichvu`
 --
 
 CREATE TABLE `dichvu` (
-  `ID` int(11) NOT NULL,
+  `MaDichVu` int(11) NOT NULL,
   `TenDichVu` varchar(50) NOT NULL,
-  `DonGia` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `DonGia` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `dichvu`
+-- Dumping data for table `dichvu`
 --
 
-INSERT INTO `dichvu` (`ID`, `TenDichVu`, `DonGia`) VALUES
-(1, 'Extrabed', '300000'),
-(2, 'Xe đưa đón sân bay', '250000');
+INSERT INTO `dichvu` (`MaDichVu`, `TenDichVu`, `DonGia`) VALUES
+(1, 'Extrabed', 300000),
+(2, 'Xe đưa đón sân bay', 250000);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `hoadon`
+-- Table structure for table `hoadon`
 --
 
 CREATE TABLE `hoadon` (
   `MaHoaDon` int(11) NOT NULL,
-  `MaKhachHang` int(11) NOT NULL,
-  `MaDatPhong` int(11) NOT NULL,
-  `MaDatMon` int(11) NOT NULL,
-  `TongTien` int(11) NOT NULL,
-  `NgayLap` date NOT NULL,
-  `MaGiamGia` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `MaPDP` int(11) NOT NULL,
+  `TongTien` varchar(50) NOT NULL,
+  `TrangThai` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hoadon`
+--
+
+INSERT INTO `hoadon` (`MaHoaDon`, `MaPDP`, `TongTien`, `TrangThai`) VALUES
+(1, 1, '3050000', 'Chờ thanh toán');
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `monan`
+-- Table structure for table `monan`
 --
 
 CREATE TABLE `monan` (
@@ -112,49 +128,65 @@ CREATE TABLE `monan` (
   `GiaMon` int(11) NOT NULL,
   `IMG` varchar(200) NOT NULL,
   `PhanLoai` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Đang đổ dữ liệu cho bảng `monan`
---
-
-INSERT INTO `monan` (`MaMonAn`, `TenMon`, `GiaMon`, `IMG`, `PhanLoai`) VALUES
-(1, 'Bún riêu cua', 35000, 'bunrieucua.jpg', 'Món mặn'),
-(2, 'Phở bò', 40000, 'phobo.jpg', 'Món mặn'),
-(3, 'Bánh mì gà', 25000, 'banhmiga.jpg', 'Món mặn'),
-(4, 'Gỏi cuốn', 30000, 'goicuon.jpg', 'Món ngon'),
-(5, 'Bánh xèo', 35000, 'banhx.jpg', 'Món ngon'),
-(6, 'Cơm rang', 32000, 'comrang.jpg', 'Món mặn'),
-(7, 'Mì Quảng', 38000, 'miquang.jpg', 'Món mặn'),
-(8, 'Bánh tráng trộn', 25000, 'banhtrangtron.jpg', 'Món ngon');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `phieugiamgia`
+-- Table structure for table `phieudatphong`
+--
+
+CREATE TABLE `phieudatphong` (
+  `MaPDP` int(11) NOT NULL,
+  `MaKhachHang` int(11) NOT NULL,
+  `TongTien` varchar(50) NOT NULL,
+  `ThanhToanTruoc` varchar(50) NOT NULL,
+  `PhuongThucThanhToan` varchar(50) NOT NULL,
+  `NgayTT` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `phieudatphong`
+--
+
+INSERT INTO `phieudatphong` (`MaPDP`, `MaKhachHang`, `TongTien`, `ThanhToanTruoc`, `PhuongThucThanhToan`, `NgayTT`) VALUES
+(1, 1, '3050000', '305000', 'Thẻ Visa, Master Card', '2023-10-17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phieudichvu`
+--
+
+CREATE TABLE `phieudichvu` (
+  `MaPDV` int(11) NOT NULL,
+  `MaPDP` int(11) NOT NULL,
+  `TongTien` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `phieudichvu`
+--
+
+INSERT INTO `phieudichvu` (`MaPDV`, `MaPDP`, `TongTien`) VALUES
+(1, 1, '550000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phieugiamgia`
 --
 
 CREATE TABLE `phieugiamgia` (
-  `MaGiamGia` varchar(25) NOT NULL,
-  `TenMaGiamGia` varchar(100) NOT NULL,
-  `ChietKhau` int(11) NOT NULL,
-  `TinhKhaDung` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Đang đổ dữ liệu cho bảng `phieugiamgia`
---
-
-INSERT INTO `phieugiamgia` (`MaGiamGia`, `TenMaGiamGia`, `ChietKhau`, `TinhKhaDung`) VALUES
-('0', 'Không giảm giá', 0, 1),
-('uudaihe2023', 'Ưu đãi mùa hè 2023, ...', 7, 1),
-('uudaithu2022', 'Ưu đãi mùa thu năm 2022', 5, 0),
-('uudaixuan2023', 'ưu đãi mùa xuân năm 2023', 10, 1);
+  `MaGiamGia` int(11) NOT NULL,
+  `TenMaGiamGia` int(11) NOT NULL,
+  `ChietKhau` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `phong`
+-- Table structure for table `phong`
 --
 
 CREATE TABLE `phong` (
@@ -169,30 +201,30 @@ CREATE TABLE `phong` (
   `TamNhin` varchar(50) NOT NULL,
   `MoTa` varchar(500) NOT NULL,
   `TrangThai` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `phong`
+-- Dumping data for table `phong`
 --
 
 INSERT INTO `phong` (`MaPhong`, `LoaiPhong`, `KieuPhong`, `SLMax`, `LoaiGiuong`, `GiaPhong`, `IMG`, `DienTich`, `TamNhin`, `MoTa`, `TrangThai`) VALUES
-(101, 'Deluxe', 'Deluxe Single', 2, '1 giường đơn', '2500000', '../img/roomstyle/junior_suite.jpg', '32m', '14:12', '0', '0'),
-(102, 'Deluxe', 'Deluxe Twin', 3, '2 giường đơn', '3200000', '../img/allroom/Deluxe_room1.jpg', '42m', 'Ra biển ', '0', '0'),
-(103, 'Deluxe', 'Deluxe Double', 3, '1 giường đôi và 1 giường đơn', '3200000', '../img/allroom/family_room.jpg', '42m', '12:10', '0', '0'),
-(104, 'Deluxe', 'Deluxe Triple', 4, '1 giường đôi và 2 giường đơn', '5800000', '../img/allroom/family_room1.jpg', '45m', '12:12', '0', '0'),
-(105, 'Executive', 'Executive Twin', 3, '2 giường đơn', '8600000', '../img/allroom/Junior_suite.jpg', '50m', '14:12', '0', '0'),
-(201, 'Executive', 'Executive Double', 3, '1 giường đôi và 1 giường đơn', '9400000', '../img/allroom/Single_room.jpg', '50m', '14:10', '0', '0'),
-(202, 'Executive', 'Executive Triple', 4, '1 giường đôi và 2 giường đơn', '10100000', '../img/allroom/Standard_room.jpg', '56m', '12:10', '0', '0'),
-(203, 'Executive', 'Executive Connecting', 6, '2 giường đôi và 2 giường đơn', '18200000', '../img/allroom/Standard_room1.jpg', '64m', '14:12', '0', '0'),
-(204, 'Suite', 'Suite Twin', 3, '2 giường đơn', '18900000', '../img/allroom/Superior_suite.jpg', '56m', '14:10', '0', '0'),
-(205, 'Suite', 'Suite Double', 3, '1 giường đôi và 1 giường đơn', '20700000', '../img/roomstyle/double_room.jpg', '56m', '12:10', '0', '0'),
-(301, 'Suite', 'Suite Triple', 4, '1 giường đôi và 2 giường đơn', '22200000', '../img/roomstyle/small_suite.jpg', '65m', '12:12', '0', '0'),
-(302, 'Suite', 'Suite Connecting', 6, '2 giường đôi và 2 giường đơn', '39500000', '../img/roomstyle/junior_suite.jpg', '72m', '14:10', '0', '0');
+(1, 'Deluxe ', 'Deluxe Single', 2, '1 giường đơn', '2500000', '../img/roomstyle/junior_suite.jpg', '32m', '14:12', '0', 'Đầy'),
+(2, 'Deluxe ', 'Deluxe Twin', 3, '2 giường đơn', '3200000', '../img/allroom/Deluxe_room1.jpg', '42m', 'Ra biển ', '0', 'Trống'),
+(3, 'Deluxe ', 'Deluxe Double', 3, '1 giường đôi và 1 giường đơn', '3200000', '../img/allroom/family_room.jpg', '42m', '12:10', '0', 'Trống'),
+(4, 'Deluxe ', 'Deluxe Triple', 4, '1 giường đôi và 2 giường đơn', '5800000', '../img/allroom/family_room1.jpg', '45m', '12:12', '0', 'Trống'),
+(5, 'Executive ', 'Executive Twin', 3, '2 giường đơn', '8600000', '../img/allroom/Junior_suite.jpg', '50m', '14:12', '0', 'Trống'),
+(6, 'Executive ', 'Executive Double', 3, '1 giường đôi và 1 giường đơn', '9400000', '../img/allroom/Single_room.jpg', '50m', '14:10', '0', 'Trống'),
+(7, 'Executive ', 'Executive Triple', 4, '1 giường đôi và 2 giường đơn', '10100000', '../img/allroom/Standard_room.jpg', '56m', '12:10', '0', 'Trống'),
+(8, 'Executive ', 'Executive Connecting', 6, '2 giường đôi và 2 giường đơn', '18200000', '../img/allroom/Standard_room1.jpg', '64m', '14:12', '0', 'Trống'),
+(9, 'Suite ', 'Suite Twin', 3, '2 giường đơn', '18900000', '../img/allroom/Superior_suite.jpg', '56m', '14:10', '0', 'Trống'),
+(10, 'Suite ', 'Suite Double', 3, '1 giường đôi và 1 giường đơn', '20700000', '../img/roomstyle/double_room.jpg', '56m', '12:10', '0', 'Trống'),
+(11, 'Suite ', 'Suite Triple', 4, '1 giường đôi và 2 giường đơn', '22200000', '../img/roomstyle/small_suite.jpg', '65m', '12:12', '0', 'Trống'),
+(12, 'Suite ', 'Suite Connecting', 6, '2 giường đôi và 2 giường đơn', '39500000', '../img/roomstyle/junior_suite.jpg', '72m', '14:10', '0', 'Trống');
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `quanlytaikhoan`
+-- Table structure for table `quanlytaikhoan`
 --
 
 CREATE TABLE `quanlytaikhoan` (
@@ -211,14 +243,13 @@ CREATE TABLE `quanlytaikhoan` (
 
 INSERT INTO `quanlytaikhoan` (`ID`, `HoTen`, `SDT`, `Email`, `CMND`, `PassWord`, `AccessToken`) VALUES
 (1, 'Admin', '0123456789', 'admin@gmail.com', 343025555, 'admin', NULL),
-(46, 'Trường Bá', 'null', '7steam.work@gmail.com', 0, '123', '105238185890006122819'),
-(47, 'Cát Trường Trương Bá', 'null', 'truongtg2k@gmail.com', 0, '123', '109408661915133195138'),
-(50, 'Cát Trường Trương Bá', '0336004076', 'batruong1704@gmail.com', 123456789, '0123456789', '108617347685033795623');
-
+(50, 'Cát Trường Trương Bá', '0336004076', 'batruong1704@gmail.com', 123456789, '0123456789', '108617347685033795623'),
+(72, 'Cát Trường Trương Bá', '0336004076', 'truongtg2k@gmail.com', 123456789, 'admin123', '109408661915133195138'),
+(73, 'Trường Bá', '', '7steam.work@gmail.com', 0, '', '105238185890006122819');
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `taikhoanadmin`
+-- Table structure for table `taikhoanadmin`
 --
 
 CREATE TABLE `taikhoanadmin` (
@@ -227,10 +258,10 @@ CREATE TABLE `taikhoanadmin` (
   `SDT` varchar(50) NOT NULL,
   `Email` varchar(50) NOT NULL,
   `PassWord` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `taikhoanadmin`
+-- Dumping data for table `taikhoanadmin`
 --
 
 INSERT INTO `taikhoanadmin` (`ID`, `HoTen`, `SDT`, `Email`, `PassWord`) VALUES
@@ -238,120 +269,187 @@ INSERT INTO `taikhoanadmin` (`ID`, `HoTen`, `SDT`, `Email`, `PassWord`) VALUES
 (2, 'thuydung', '0394407543', 'dung@gmail.com', '1');
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `chitietdatmon`
+-- Indexes for table `chitietdatmon`
 --
 ALTER TABLE `chitietdatmon`
   ADD PRIMARY KEY (`MaDatMon`);
 
 --
--- Chỉ mục cho bảng `chitietdatphong`
+-- Indexes for table `chitietdatphong`
 --
 ALTER TABLE `chitietdatphong`
-  ADD PRIMARY KEY (`MaDatPhong`);
+  ADD PRIMARY KEY (`MaCTDP`),
+  ADD KEY `mkl3` (`MaPDP`),
+  ADD KEY `mkl4` (`MaPhong`);
 
 --
--- Chỉ mục cho bảng `chitietdichvu`
+-- Indexes for table `chitietdichvu`
 --
 ALTER TABLE `chitietdichvu`
-  ADD PRIMARY KEY (`MaDatDichVu`);
+  ADD PRIMARY KEY (`MaCTDV`),
+  ADD KEY `mkl1` (`MaPDV`),
+  ADD KEY `mkl2` (`MaDichVu`);
 
 --
--- Chỉ mục cho bảng `dichvu`
+-- Indexes for table `dichvu`
 --
 ALTER TABLE `dichvu`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`MaDichVu`);
 
 --
--- Chỉ mục cho bảng `hoadon`
+-- Indexes for table `hoadon`
 --
 ALTER TABLE `hoadon`
-  ADD PRIMARY KEY (`MaHoaDon`);
+  ADD PRIMARY KEY (`MaHoaDon`),
+  ADD KEY `MaPDP` (`MaPDP`);
 
 --
--- Chỉ mục cho bảng `monan`
+-- Indexes for table `monan`
 --
 ALTER TABLE `monan`
   ADD PRIMARY KEY (`MaMonAn`);
 
 --
--- Chỉ mục cho bảng `phieugiamgia`
+-- Indexes for table `phieudatphong`
+--
+ALTER TABLE `phieudatphong`
+  ADD PRIMARY KEY (`MaPDP`),
+  ADD KEY `MaKhachHang` (`MaKhachHang`);
+
+--
+-- Indexes for table `phieudichvu`
+--
+ALTER TABLE `phieudichvu`
+  ADD PRIMARY KEY (`MaPDV`),
+  ADD KEY `MaPDP` (`MaPDP`);
+
+--
+-- Indexes for table `phieugiamgia`
 --
 ALTER TABLE `phieugiamgia`
   ADD PRIMARY KEY (`MaGiamGia`);
 
 --
--- Chỉ mục cho bảng `phong`
+-- Indexes for table `phong`
 --
 ALTER TABLE `phong`
   ADD PRIMARY KEY (`MaPhong`);
 
 --
--- Chỉ mục cho bảng `quanlytaikhoan`
+-- Indexes for table `quanlytaikhoan`
 --
 ALTER TABLE `quanlytaikhoan`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Chỉ mục cho bảng `taikhoanadmin`
+-- Indexes for table `taikhoanadmin`
 --
 ALTER TABLE `taikhoanadmin`
   ADD PRIMARY KEY (`ID`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT cho bảng `chitietdatmon`
+-- AUTO_INCREMENT for table `chitietdatmon`
 --
 ALTER TABLE `chitietdatmon`
   MODIFY `MaDatMon` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT cho bảng `chitietdatphong`
+-- AUTO_INCREMENT for table `chitietdatphong`
 --
 ALTER TABLE `chitietdatphong`
-  MODIFY `MaDatPhong` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `MaCTDP` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT cho bảng `chitietdichvu`
+-- AUTO_INCREMENT for table `chitietdichvu`
 --
 ALTER TABLE `chitietdichvu`
-  MODIFY `MaDatDichVu` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `MaCTDV` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT cho bảng `dichvu`
+-- AUTO_INCREMENT for table `dichvu`
 --
 ALTER TABLE `dichvu`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `MaDichVu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT cho bảng `hoadon`
+-- AUTO_INCREMENT for table `hoadon`
 --
 ALTER TABLE `hoadon`
-  MODIFY `MaHoaDon` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `MaHoaDon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT cho bảng `monan`
+-- AUTO_INCREMENT for table `monan`
 --
 ALTER TABLE `monan`
-  MODIFY `MaMonAn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `MaMonAn` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT cho bảng `quanlytaikhoan`
+-- AUTO_INCREMENT for table `phieudatphong`
+--
+ALTER TABLE `phieudatphong`
+  MODIFY `MaPDP` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `phieudichvu`
+--
+ALTER TABLE `phieudichvu`
+  MODIFY `MaPDV` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `quanlytaikhoan`
 --
 ALTER TABLE `quanlytaikhoan`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
--- AUTO_INCREMENT cho bảng `taikhoanadmin`
+-- AUTO_INCREMENT for table `taikhoanadmin`
 --
 ALTER TABLE `taikhoanadmin`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `chitietdatphong`
+--
+ALTER TABLE `chitietdatphong`
+  ADD CONSTRAINT `mkl3` FOREIGN KEY (`MaPDP`) REFERENCES `phieudatphong` (`MaPDP`),
+  ADD CONSTRAINT `mkl4` FOREIGN KEY (`MaPhong`) REFERENCES `phong` (`MaPhong`);
+
+--
+-- Constraints for table `chitietdichvu`
+--
+ALTER TABLE `chitietdichvu`
+  ADD CONSTRAINT `mkl1` FOREIGN KEY (`MaPDV`) REFERENCES `phieudichvu` (`MaPDV`),
+  ADD CONSTRAINT `mkl2` FOREIGN KEY (`MaDichVu`) REFERENCES `dichvu` (`MaDichVu`);
+
+--
+-- Constraints for table `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD CONSTRAINT `hoadon_ibfk_1` FOREIGN KEY (`MaPDP`) REFERENCES `phieudatphong` (`MaPDP`);
+
+--
+-- Constraints for table `phieudatphong`
+--
+ALTER TABLE `phieudatphong`
+  ADD CONSTRAINT `phieudatphong_ibfk_1` FOREIGN KEY (`MaKhachHang`) REFERENCES `quanlytaikhoan` (`ID`);
+
+--
+-- Constraints for table `phieudichvu`
+--
+ALTER TABLE `phieudichvu`
+  ADD CONSTRAINT `phieudichvu_ibfk_1` FOREIGN KEY (`MaPDP`) REFERENCES `phieudatphong` (`MaPDP`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
