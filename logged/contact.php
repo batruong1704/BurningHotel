25076416f419f15df2a55bc1eceacd07da5f5e11
Dung Nguyễn <nguyenthuydungtstb@gmail.com>
2023-10-28 22:40:08 +0700

    commit 2

--- logged/contact.php
+++ logged/contact.php
@@ -95,13 +95,8 @@
     <?php
 
     use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-    use PHPMailer\PHPMailer\SMTP;
-
-    require '../vendor/phpmailer/phpmailer/src/Exception.php';
-    require '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
-    require '../vendor/phpmailer/phpmailer/src/SMTP.php';
     require '../vendor/autoload.php';
+
     if (isset($_POST['submit'])) {
         $ten = $_POST['ten'];
         $email = $_POST['email'];
