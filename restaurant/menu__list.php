6b102ab6b590724768729a4ebd0f1e411abf1b67
TienQuang <trinhtienquang2002@gmail.com>
2023-11-14 21:35:03 +0700

    fix pagination menu_list

--- restaurant/menu__list.php
+++ restaurant/menu__list.php
@@ -69,27 +69,52 @@
                     echo '</div>';
                     echo '</div>';
                 }
-
-
+                echo'<nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li class="page-item">';
+                        if ($current_page > 1) {
+                            $prev_page = $current_page - 1;
+                            echo '<a href="?category=' . $category . '&page=' . $prev_page . '" class="page-link">Previous</a>';
+                        }
+                echo    '</li>
+                        <li class="page-item d-flex">';
+                        $total_pages = ceil($total_rows / $items_per_page);
+                        for ($i = 1; $i <= $total_pages; $i++) {
+                            echo '<a href="?category=' . $category . '&page=' . $i . '" class="page-link">' . $i . '</a>';
+                        }
+                echo    '</li>
+                        
+                <li class="page-item">';
+                        if ($result->num_rows == $items_per_page) {
+                            $next_page = $current_page + 1;
+                            echo '<a href="?category=' . $category . '&page=' . $next_page . '" class="page-link">Next</a>';
+                        } else {
+                            if (isset($next_page)) {
+                                echo '<style> a[href="?category=' . $category . '&page=' . $next_page . '"] { display: none; } </style>';
+                            }
+                        }
+                echo    '</li>
+                    </ul>
+                </nav>';
                 // TODO: Lay du lieu cac page
-                if ($current_page > 1) {
-                    $prev_page = $current_page - 1;
-                    echo '<a href="?category=' . $category . '&page=' . $prev_page . '"><</a>';
-                }
+                // if ($current_page > 1) {
+                //     $prev_page = $current_page - 1;
+                //     echo '<a href="?category=' . $category . '&page=' . $prev_page . '"><</a>';
+                // }
 
-                $total_pages = ceil($total_rows / $items_per_page);
-                for ($i = 1; $i <= $total_pages; $i++) {
-                    echo '<a href="?category=' . $category . '&page=' . $i . '">' . $i . '</a>';
-                }
+                // $total_pages = ceil($total_rows / $items_per_page);
+                // for ($i = 1; $i <= $total_pages; $i++) {
+                //     echo '<a href="?category=' . $category . '&page=' . $i . '">' . $i . '</a>';
+                // }
 
-                if ($result->num_rows == $items_per_page) {
-                    $next_page = $current_page + 1;
-                    echo '<a href="?category=' . $category . '&page=' . $next_page . '">></a>';
-                } else {
-                    if (isset($next_page)) {
-                        echo '<style> a[href="?category=' . $category . '&page=' . $next_page . '"] { display: none; } </style>';
-                    }
-                }
+                // if ($result->num_rows == $items_per_page) {
+                //     $next_page = $current_page + 1;
+                //     echo '<a href="?category=' . $category . '&page=' . $next_page . '">></a>';
+                // } else {
+                //     if (isset($next_page)) {
+                //         echo '<style> a[href="?category=' . $category . '&page=' . $next_page . '"] { display: none; } </style>';
+                //     }
+                // }
             } else {
                 echo "0 kết quả";
             }
